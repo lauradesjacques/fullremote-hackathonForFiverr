90bddf9796c10fdb9a141b4f1a6ae79b341690c9
Mathieu Ponce <poncemathieu@gmail.com>
2021-06-29 14:17:16 +0200

    mise en place du service

---
+++ src/app/common/freelancer.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FreelancerService {
+  public urlApi: string = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  public getFreelancer(): Observable<any> {
+    return this.http.get(this.urlApi + '/read?id=1');
+  }
+}
