848ffee5542186bce85c6079114da78c1da16c60
Mathieu Ponce <poncemathieu@gmail.com>
2021-06-30 12:39:35 +0200

    ok

---
+++ src/app/page-reponse/page-reponse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-reponse',
+  templateUrl: './page-reponse.component.html',
+  styleUrls: ['./page-reponse.component.css']
+})
+export class PageReponseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
